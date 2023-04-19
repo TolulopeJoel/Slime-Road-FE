17bcb127cad6b787b52a685e06024fc4cc346f30
Tolu Joel <toluisjoel@gmail.com>
2023-04-19 18:43:27 +0100

    Adjusted sidebar and removed redundant code

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -4,15 +4,14 @@ import React from "react";
 export default function Sidebar() {
     return (
         <>
-            <div class="sidebar">
-                <a class="active" href="#home">Home</a>
-                <a href="#news">News</a>
-                <a href="#contact">Contact</a>
-                <a href="#about">About</a>
-            </div>
-
-            <div class="content">
-                <h1>Tolulope Joel</h1>
+            <div className="sidebar border border-1 border-dark">
+                <div className="logo">
+                    <a href=""><h1>Slime Road</h1></a>
+                </div>
+                <a href='' className="active">Home</a>
+                <a href=''>Products</a>
+                <a href=''>Library</a>
+                <a href=''>Payouts</a>
             </div>
         </>
     )
