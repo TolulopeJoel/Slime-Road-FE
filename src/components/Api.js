f42ef6a02c02a86d49aacdede125cdab97cd4e75
Tolu Joel <toluisjoel@gmail.com>
2023-05-03 03:42:57 +0100

    Modified api component

--- src/components/Api.js
+++ src/components/Api.js
@@ -21,11 +21,36 @@ api.interceptors.response.use(
   function (response) {
     return response;
   },
+  
   function (error) {
     // Check if the error is due to an invalid or expired token
     if (error.response.status === 401) {
-      // Redirect the user to the sign-in page
-      window.location.href = '/login';
+      const refreshToken = localStorage.getItem("refresh_token");
+      if (!refreshToken) {
+        // Redirect the user to the sign-in page if there's no refresh token
+        localStorage.removeItem("access_token");
+        window.location.href = "/login";
+        return Promise.reject(error);
+      }
+
+      // Try to refresh the access token using the refresh token
+      return axios
+        .post(`${apiBaseURL}/auth/token/refresh/`, { refresh: refreshToken })
+        .then((response) => {
+          // Update the access token in local storage
+          localStorage.setItem("access_token", response.data.acess);
+          // Retry the original request with the new access token
+          const config = error.config;
+          config.headers["Authorization"] = `Bearer ${response.data.access}`;
+          return axios.request(config);
+        })
+        .catch((error) => {
+          // Redirect the user to the sign-in page if the refresh token is invalid
+          localStorage.removeItem("access_token");
+          localStorage.removeItem("refresh_token");
+          window.location.href = "/login";
+          return Promise.reject(error);
+        });
     }
     return Promise.reject(error);
   }
